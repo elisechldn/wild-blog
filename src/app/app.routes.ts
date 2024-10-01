9fc1d958db24a15139bdfc99530869ed84c3e511
elisechldn <elise.chaldjian@gmail.com>
2024-10-02 00:22:10 +0200

    Added a Contact form and the validator errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'article/id/:id', component: ArticleComponentComponent },
+    { path:'contact', component: ContactFormComponent},
     { path:'**', component: NotFoundComponent },
 ];
